e53a35646b35826ff8449207d1189c29ace8b663
Abul Monsur <154686044+monsur28@users.noreply.github.com>
2024-04-30 23:36:27 +0600

    Delete Done

--- index.js
+++ index.js
@@ -67,6 +67,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/art/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await artCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.post("/art", async (req, res) => {
       const newArt = req.body;
       console.log(newArt);
